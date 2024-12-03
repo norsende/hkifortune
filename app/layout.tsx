155a15279e2521cf4ac0a61ad1d33cb38994d6ec
Niko Heikura <niko.heikura@futurice.com>
2024-12-03 21:46:03 +0200

    Changed site title and desc.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { ReactNode } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Aimo Apuri Demo",
-  description: "Aimo Apuri Demo",
+  title: "Helsinki Fortune & Fate Oracle",
+  description: "Helsinki Fortune & Fate Oracle",
 };
 
 interface RootLayoutProps {
